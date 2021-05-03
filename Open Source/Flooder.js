531c6a9492045a871452f74c17fc9a41f1149568
seanv999 <79374771+seanv999@users.noreply.github.com>
2021-05-03 10:46:32 -0400

    Add files via upload

---
+++ Open Source/Flooder.js
@@ -0,0 +1,33 @@
+//defining vars
+let nameOfbot = prompt("Name of bot: ")
+let gameId = prompt("Enter Game Pin: ");
+let numberOfBots = prompt("How many Bots: ")
+let botsToJoin;
+
+//Credit
+console.log('%c Created by Sean v  :D', 'background: #222; color: yellow');
+
+
+
+// Making a for loop for flood
+for(botsToJoin = 0; botsToJoin < numberOfBots; botsToJoin++)
+
+fetch("https://api.blooket.com/api/firebase/join", {
+  "headers": {
+    "accept": "application/json, text/plain, */*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json;charset=UTF-8",
+    "sec-ch-ua": "\"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://www.blooket.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  //adding vars to body for correct values
+  "body": "{\"id\":\"" + gameId+ "\",\"name\":\"" + nameOfbot + botsToJoin+ "\"}",
+  "method": "PUT",
+  "mode": "cors",
+  "credentials": "omit"
+});
